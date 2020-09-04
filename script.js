06bb718d8cac39693af35e545bec693011a8ad95
DCLucas95 <69066157+DCLucas95@users.noreply.github.com>
2020-09-04 17:11:13 +1000

    basic html and css, questions written with answers

---
+++ script.js
@@ -0,0 +1,13 @@
+const quizcontainer = document.getElementById('jsQuiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+function  buildQuiz(){}
+
+function showResults(){}
+
+//Show the quiz
+buildQuiz();
+
+//show score when submit clicked
+submitButton.addEventListener('click', showResults);
