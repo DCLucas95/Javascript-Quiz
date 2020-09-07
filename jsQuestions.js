15c9e639af31089ea847ce18a6cc99700cf01e1d
DCLucas95 <69066157+DCLucas95@users.noreply.github.com>
2020-09-07 10:10:58 +1000

    added questions and timer /currently marking all answers as wrong - need to fix

--- jsQuestions.js
+++ jsQuestions.js
@@ -2,217 +2,114 @@ var quizQuestions = [
 
 {
 question:"Inside which HTML element do we put the JavaScript?",
-answers: {
-a: "<js>",
-b: "<javascript>",
-c: "<scripting>",
-d: "<script>"
-},
-correctAnswer: "d"
-},
-
-
-{
-question:"Where is the correct place to instert a Javascript?",
-answers: {
-a: "The <head> section",
-b: "The <body> section",
-c: "Both the <head> and the <body> section are correct"
-},
-correctAnswer: "c"
-},
-
-
-{
-question:"What is the correct syntax for referring to an external script called "abc.js"?",
-answers: {
-a: "<script name= >",
-b: "<script src= >",
-c: "<script href= >"
-},
-correctAnswer: "b"
-},
-
-
-{
-question:"The external JavaScript file must contain the <script> tag.",
-answers: {
-a: "True",
-b: "False"
-},
-correctAnswer: "b"
-},
-
-
-{
-question:"What type of javascript brings up a box that must be closed by the user to proceed?",
-answers: {
-a: "alert",
-b: "msg",
-c: "msgBox",
-d: "alertBox"
-},
-correctAnswer: "a"
+answers: [
+"<js>",
+"<javascript>",
+"<scripting>",
+"<script>"
+],
+correctAnswer: "3"
 },
 
 
 {
 question:"How do you create a function in JavaScript?",
-answers: {
-a: "function = myFunction()",
-b: "function myFunction()",
-c: "function:myFunction()"
-},
-correctAnswer: "a"
+answers: [
+"function = myFunction()",
+"function myFunction()",
+"function:myFunction()"
+],
+correctAnswer: "0"
 },
 
 
 {
 question:"How to write an IF statement in JavaScript?",
-answers: {
-a: "if (i==5)",
-b: "if i=5 then",
-c: "if i==5 then",
-d: "if i=5"
-},
-correctAnswer: "a"
+answers: [
+"if (i==5)",
+"if i=5 then",
+"if i==5 then",
+"if i=5"
+],
+correctAnswer: "0"
 },
 
 
 {
 question:"How to write an IF statement for executing some code if i is NOT equal to 5?",
-answers: {
-a: "if i<>5",
-b: "if (i!=5)",
-c: "if (i<>5)",
-d: "if i=!5 then"
-},
-correctAnswer: "b"
-},
-
-
-{
-question:"How does a WHILE loop start?",
-answers: {
-a: "while(i <= 10;i++)",
-b: "while i = 1 to 10",
-c: "while(i<=10)"
-},
-correctAnswer: "c"
+answers: [
+"if i<>5",
+"if (i!=5)",
+"if (i<>5)",
+"if i=!5 then"
+],
+correctAnswer: 1
 },
 
 
 {
 question:"How does a FOR loop start?",
-answers: {
-a: "for i = 1 to 5",
-b: "for (i=0;i<=5)",
-c: "for (i<=5;i++)",
-d: "for (i=0; i<=5;i++)"
-},
-correctAnswer: "d"
-},
-
-
-{
-question:"How can you add a comment in a JavaScript?",
-answers: {
-a: "'This is a comment",
-b: "//This is a comment",
-c: "<!--This is a comment-->",
-d: ""
-},
-correctAnswer: "b"
-},
-
-
-{
-question:"What is a javascript array?",
-answers: {
-a: "A variable",
-b: "A constant",
-c: "A variable containg more than one value",
-},
-correctAnswer: "c"
+answers: [
+"for i = 1 to 5",
+"for (i=0;i<=5)",
+"for (i<=5;i++)",
+"for (i=0; i<=5;i++)"
+],
+correct: 3
 },
 
 
 {
 question:"JavaScript is the same as Java.",
-answers: {
-a: "True",
-b: "False"
-},
-correctAnswer: "b"
+answers: [
+"True",
+"False"
+],
+correct: 1
 },
 
 
 {
 question:"How can you detect the client's browser name?",
-answers: {
-a: "navigator.appName",
-b: "browser.name",
-c: "client.navName"
-},
-correctAnswer: "a"
+answers: [
+"navigator.appName",
+"browser.name",
+"client.navName"
+],
+correct: 0
 },
 
 
 {
 question:"Which event occurs when the user clicks on an HTML element?",
-answers: {
-a: "onchange",
-b: "onmouseclick",
-c: "onclick",
-d: "onmouseover"
-},
-correctAnswer: "c"
+answers: [
+"onchange",
+"onmouseclick",
+"onclick",
+"onmouseover"
+],
+correct: 2
 },
 
 
 {
 question:"How do you declare a JavaScript variable?",
-answers: {
-a: "var carName;",
-b: "v carName;",
-c: "variable carName"
-},
-correctAnswer: "a"
-},
-
-
-{
-question:"Which operator is used to assign a value to a variable?",
-answers: {
-a: "*",
-b: "=",
-c: "-",
-d: "x"
-},
-correctAnswer: "b"
-},
-
-
-{
-question:"What will the following code return: Boolean(10 > 9)",
-answers: {
-a: "NaN",
-b: "false",
-c: "true"
-},
-correctAnswer: "c"
+answers: [
+"var carName;",
+"v carName;",
+"variable carName"
+],
+correct: 0
 },
 
 
 {
 question:"is JavaScript case sensitive?",
-answers: {
-a: "Yes",
-b: "No"
-},
-correctAnswer: "a"
+answers: [
+"Yes",
+"No"
+],
+correct: 0
 },
 
 ]
-
-console.log(quizQuestions)
--- script.js
+++ script.js
@@ -1,13 +1,74 @@
-const quizcontainer = document.getElementById('jsQuiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+//Source of questions for the quiz
+var questions = quizQuestions
 
-function  buildQuiz(){}
 
-function showResults(){}
+//Identifier of button to start the quiz
+var startButton = document.querySelector(".startButton")
 
-//Show the quiz
-buildQuiz();
+// index of the current question and aswers in the questions array (start from the first)
+var currentQuestion = 0;
+var questionHtml = document.querySelector('#question');
+var answer1 = document.querySelector('#answer1');
+var answer2 = document.querySelector('#answer2');
+var answer3 = document.querySelector('#answer3');
 
-//show score when submit clicked
-submitButton.addEventListener('click', showResults);
+//countdown timer
+var countdowntimer
+var secondsLeft = 60;
+
+function startTimer() {
+
+  renderQuestion();
+
+  countdowntimer = setInterval(function() {
+    secondsLeft=secondsLeft-1
+
+    var showTimer = document.getElementById("countdownTimer");
+    showTimer.textContent = secondsLeft
+    
+    if(secondsLeft === 0) {
+      clearInterval(countdowntimer);
+      window.location.href = "scores.html";
+    }
+  }, 1000);
+}
+
+//Button to start Quiz and Timer
+startButton.addEventListener("click", startTimer);
+
+// this function will display question and possible answers
+function renderQuestion() {
+  var questionObject = questions[currentQuestion];
+  //displaying question
+  questionHtml.textContent = questionObject.question;
+
+  //displaying answers
+  answer1.textContent = questionObject.answers[0];
+  answer2.textContent = questionObject.answers[1];
+  answer3.textContent = questionObject.answers[2];
+  answer4.textContent = questionObject.answers[3];
+}
+
+// when the user clicks on one of the answers this function will be executed
+function selectAnswer(answerNumber) {
+  console.log('user answered ' + answerNumber);
+  var questionObject = questions[currentQuestion];
+
+  if (questionObject.correct === answerNumber) {
+    console.log('answer is correct!');
+    //score ++
+  } else {
+    console.log('answer is wrong!');
+    secondsLeft -=5
+  }
+  console.log(currentQuestion, questions.length);
+  // display next question if there is one
+  if (currentQuestion >= questions.length - 1) {
+    console.log('that was the last question');
+    window.location.href = "scores.html";
+  } else {
+    currentQuestion++;
+    renderQuestion();
+  }
+}
+
